2a26251ff21285927e9a4c68a55331af35b9f9d7
Alvin Wong <alvinhochun@gmail.com>
2020-06-27 16:30:53 +0800

    Rhai demo: Playground build fc6600a

---
+++ rhai-demo/78.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[78],{36:function(n,r,e){"use strict";e.r(r),r.default='print("The result should be 182:");\r\n\r\nlet x = 12 + 34 * 5;\r\nprint(x);\r\n'}}]);
